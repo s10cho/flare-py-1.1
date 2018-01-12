0c3d02a61dda07d3653bdf4ec2f4388083e78c19
s10cho <s10cho@naver.com>
2018-01-12 13:30:06 +0900

    flare

--- flare.py
+++ flare.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import importlib
+import traceback
 
 def main():
     args = sys.argv[1:]
@@ -23,10 +24,9 @@ def main():
             Class = getattr(module, ClassName)
             instance = Class()
             instance.run()
-        except ModuleNotFoundError as e:
-            print('No Command is ', e)
         except Exception as e:
             print(e)
+            print(traceback.format_exc())
 
 
 
