ac89e1287a178431890a6bd9bb01bdcd5929ed59
s10cho <s10cho@naver.com>
2018-01-17 19:27:51 +0900

    flare

--- flare_module/install/command/docker.py
+++ flare_module/install/command/docker.py
@@ -23,13 +23,9 @@ class Docker():
         , '-p {0}:{1}'.format(PORT['GATEWAY'][0], PORT['GATEWAY'][1])
         , '-p {0}:{1}'.format(PORT['WEBAPPS'][0], PORT['WEBAPPS'][1])
         , '-p {0}:{1}'.format(PORT['WEBROOT'][0], PORT['WEBROOT'][1])
-        , '-e JAVA_HOME={0}'.format('/usr/java/jdk1.8.0_151')
-        , '-e ANT_HOME={0}'.format('/usr/java/apache-ant-1.10.1')
-        , '-e ${PATH}=${PATH}:$HOME/bin:$JAVA_HOME/bin'
         , '--cpuset-cpus="0-3"'
         , '--memory=8G'
         , 'centos7/eer:1.0'
-        , 'bash'
     ]
 
     DOCKER_ANT = [
