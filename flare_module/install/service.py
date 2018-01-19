22fe5d752a1460fdb8e9ab38e0c112c1a142131a
s10cho <s10cho@naver.com>
2018-01-19 10:16:24 +0900

    flare

--- flare_module/install/service.py
+++ flare_module/install/service.py
@@ -2,6 +2,7 @@ from flare_module.install.command.setup import Setup
 from flare_module.install.command.ant import Ant
 from flare_module.install.command.maven import Maven
 from flare_module.install.command.docker import Docker
+from flare_module.install.command.logback import Logback
 
 class InstallService():
     def __init__(self):
@@ -9,11 +10,13 @@ class InstallService():
         self.maven = Maven()
         self.ant = Ant()
         self.docker = Docker()
+        self.logback = Logback()
 
     def run(self, param):
         if len(param) == 0:
             self.docker.rm()
             self.setup.settings()
+            self.logback.changeLevel()
             self.maven.database_clean()
             #self.ant.build()
             self.docker.run()
