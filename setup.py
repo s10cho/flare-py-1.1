2641aee1aa864625ccf252034ea87445915eae57
s10cho <s10cho@naver.com>
2018-01-25 11:26:00 +0900

    flare

--- setup.py
+++ setup.py
@@ -1,13 +1,16 @@
 import io
+import os
 from setuptools import find_packages, setup
 
 
 # Read in the README for the long description on PyPI
 def long_description():
-    with io.open('README.rst', 'r', encoding='utf-8') as f:
-        readme = f.read()
-    #return readme
-    return ''
+    if os.path.isfile('README.rst'):
+        with io.open('README.rst', 'r', encoding='utf-8') as f:
+            readme = f.read()
+    else:
+        readme = ''
+    return readme
 
 setup(name='flare',
       version='1.0',
