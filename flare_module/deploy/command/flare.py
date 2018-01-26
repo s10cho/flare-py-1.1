1916616a393a6a12977dcee13fbab8ce4780c399
s10cho <s10cho@naver.com>
2018-01-26 13:36:43 +0900

    flare

--- flare_module/deploy/command/flare.py
+++ flare_module/deploy/command/flare.py
@@ -2,7 +2,7 @@ from fabric.api import *
 from config import FlarePath, FlareEnv
 from decorator import before, remote
 
-#@before(remote(FlareEnv.SERVER["FLARE"]))
+@before(remote(FlareEnv.SERVER["FLARE"]))
 class FlareServer():
 
     def __init__(self): pass
