277aea21403e2aa6ba0aa968f8ba097f28729843
s10cho <s10cho@naver.com>
2018-01-26 14:01:04 +0900

    flare

--- flare_module/deploy/command/flare.py
+++ flare_module/deploy/command/flare.py
@@ -5,6 +5,7 @@ from decorator import before, remote
 #@before(remote(FlareEnv.SERVER["FLARE"]))
 class FlareServer():
     DEPLOY_TEMP_PATH = FlarePath.TEMP_HOME + '/deploy'
+    DEPLOY_TAR_NAME = 'enomix.tar'
 
     def __init__(self): pass
 
@@ -15,4 +16,5 @@ class FlareServer():
         local('rm -rf {0}'.format(self.DEPLOY_TEMP_PATH))
         local('mkdir -p {0}'.format(self.DEPLOY_TEMP_PATH))
         local('cd {0}'.format(FlarePath.ORACLE_HOME))
-        local('ls -al')
+        local('tar -cvf {0} {1}'.format(self.DEPLOY_TAR_NAME, FlarePath.ORACLE_HOME))
+        local('mv {0} {1}'.format(self.DEPLOY_TAR_NAME, self.DEPLOY_TEMP_PATH))
