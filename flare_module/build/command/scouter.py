29d7092c61864d7369b85b5b3cdc9eeef119d767
s10cho <s10cho@naver.com>
2018-02-01 17:04:42 +0900

    flare

--- flare_module/build/command/scouter.py
+++ flare_module/build/command/scouter.py
@@ -58,6 +58,6 @@ class Scouter():
 
         for scouterModule in ['/agent.java', '/agent.host']:
             scouterPath = self.SCOUTER_SETUP[0] + scouterModule
-            deployPath = self.SCOUTER_SETUP[2] + scouterModule
+            deployPath = self.SCOUTER_SETUP[2]
             local('mkdir -p {0}'.format(deployPath))
             local('cp -r {0} {1}'.format(scouterPath, deployPath))
