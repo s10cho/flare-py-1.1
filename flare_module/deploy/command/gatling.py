2c46fc86d4e61192599e7e649252b85d1350c3e6
s10cho <s10cho@naver.com>
2018-02-05 14:55:00 +0900

    flare

--- flare_module/deploy/command/gatling.py
+++ flare_module/deploy/command/gatling.py
@@ -51,6 +51,7 @@ class GatlingServer():
         # cd remote home
         with cd(FlareDeploy.REMOTE_HOME):
             run('unzip {0}'.format(self.gatlingZipName))
+            run('mv {0} gatling'.format(self.gatlingZipName[0:self.gatlingZipName.find('.')]))
             run('rm -rf {0}'.format(self.gatlingZipName))
 
 
