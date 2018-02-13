146a53372fa12b5e121d8f84b038edfc8fc932fe
s10cho <s10cho@naver.com>
2018-02-13 13:53:37 +0900

    flare

--- flare_bin/run.sh
+++ flare_bin/run.sh
@@ -2,11 +2,6 @@
 
 FLARE_HOME=$(cd "$(dirname "$0")" && cd ../;pwd)
 
-echo "Argument Count : $#"
-echo "Argument 1 : $1"
-echo "Argument 2 : $2"
-
-
 # run flare
 cd $FLARE_HOME
 python3 flare.py $*
