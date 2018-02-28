5fc6838848946d83584de97c88597e97892c260b
s10cho <s10cho@naver.com>
2018-02-28 16:20:31 +0900

    flare

---
+++ flare_bin/web.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+FLARE_HOME=$(cd "$(dirname "$0")" && cd ../;pwd)
+
+# server run
+cd $FLARE_HOME
+
+if [ -f server.pid ]; then
+	pid=`cat server.pid`
+	kill $pid
+	rm -f server.pid
+	echo "server stopped"
+else
+    PROCESS="ps -ef | grep python3 | grep server.py"
+    proc=`eval $PROCESS`
+    pid=`echo $proc | awk '{print $2}'`
+    kill $pid
+fi
+
+python3 server.py > server.log 2>&1 &
+echo $! > server.pid
