b267a86592ac044ee010ac0a6430bd9c1e358c67
s10cho <s10cho@naver.com>
2018-02-28 13:38:14 +0900

    flare

---
+++ flare_web/api/report.py
@@ -0,0 +1,29 @@
+import os
+from flask import jsonify
+from config import FlarePath
+
+
+class Report():
+
+    REPORT_ROOT = FlarePath.FLARE_RESULT + '/report'
+    REPORT_TYPE = REPORT_ROOT + '/{0}'
+    REPORT_DATE = REPORT_ROOT + '/{0}/{1}'
+
+    def list(self, path):
+        data = os.listdir(path)
+        result = {
+            'data': data
+        }
+        return jsonify(result)
+
+    def selectType(self):
+        return self.list(self.RESULT_ROOT)
+
+    def selectDate(self, type):
+        return self.list(self.REPORT_TYPE.format(type))
+
+    def selectReport(self, type, date):
+        return self.list(self.REPORT_DATE.format(type, date))
+
+    def view(self, view):
+        return view
---
+++ flare_web/server.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask.views import View
+from flask import render_template
+from tornado.wsgi import WSGIContainer
+from tornado.httpserver import HTTPServer
+from tornado.ioloop import IOLoop
+from config import FlarePath, FlareWeb
+from flare_web.api.report import Report
+
+class FlareView(View):
+    def __init__(self, template_name):
+        self.template_name = template_name
+    def dispatch_request(self):
+        return render_template(self.template_name)
+
+# Define
+app = Flask(__name__, static_folder=FlarePath.FLARE_RESULT, static_url_path='', template_folder=FlarePath.FLARE_RESULT)
+app.debug = True
+report = Report()
+
+# Router
+app.add_url_rule('/', view_func=FlareView.as_view('index', template_name='index.html'))
+
+app.add_url_rule('/api/report', view_func=report.selectType)
+app.add_url_rule('/api/report/<type>', view_func=report.selectDate)
+app.add_url_rule('/api/report/<type>/<date>', view_func=report.selectReport)
+
+# Main
+if __name__ == '__main__':
+    http_server = HTTPServer(WSGIContainer(app))
+    http_server.listen(FlareWeb.PORT)
+    print('Server Start {0}'.format(FlareWeb.PORT))
+    IOLoop.instance().start()
